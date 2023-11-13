84c76feab929aaf729815f959572ae95e8b81218
hauhppc05211 <139949373+hauhppc05211@users.noreply.github.com>
2023-11-13 15:08:19 +0700

    xóa bớt form

--- DuAn1_PeachCoffee.sql
+++ DuAn1_PeachCoffee.sql
@@ -2,7 +2,6 @@
 
 go
 use Peach_Coffee
-
 go
 IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[NhanVien]') AND type in (N'U'))
 DROP TABLE [dbo].[NhanVien]
@@ -18,9 +17,11 @@ create table NhanVien(
 	NgayVaoLam datetime,
 	GhiChu nvarchar(50),
 	GioiTinh bit,
-	TrangThai nvarchar(50),
-	ChucVu int not null
+	TrangThai bit,
+	ChucVu bit,
+	Hinh nvarchar(50)
 )
+
 
 
 go
@@ -29,8 +30,9 @@ DROP TABLE [dbo].[LoaiSanPham]
 go
 create table LoaiSanPham(
 	MaLSP nvarchar(10) primary key,
-	TenLSP nvarchar(50),
-	MoTa nvarchar(50)
+	TenLSP nvarchar(50) not null,
+	MoTa nvarchar(50),
+	TrangThai nvarchar(50)
 )
 go
 IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[KhuyenMai]') AND type in (N'U'))
@@ -43,7 +45,8 @@ create table KhuyenMai(
 	NgayBD datetime not null,
 	NgayKT datetime not null,
 	GiaKM float,
-	GhiChu nvarchar(50)
+	GhiChu nvarchar(50),
+	TrangThai bit
 )
 
 go
@@ -53,13 +56,14 @@ DROP TABLE [dbo].[SanPham]
 go
 create table SanPham(
 	MaSP nvarchar(10) primary key,
-	TenSP nvarchar(50),
+	TenSP nvarchar(50) not null,
 	SoLuong int,
 	HinhAnh nvarchar(50),
 	Gia float,
 	GhiChu nvarchar(50),
 	MaLSP nvarchar(10),
-	MaKM nvarchar(10)
+	MaKM nvarchar(10),
+	TrangThai bit,
 	foreign key (MaLSP) references LoaiSanPham(MaLSP),
 	foreign key (MaKM) references KhuyenMai(MaKM)
 )
@@ -71,11 +75,25 @@ DROP TABLE [dbo].[CaLamViec]
 go
 create table CaLamViec(
 	MaCLV nvarchar(10) primary key,
-	TenCLV nvarchar(50),
+	TenCLV nvarchar(50)  not null,
 	NhanVienTrucCa nvarchar(10),
 	Thu nvarchar(15),
 	GhiChu nvarchar(50),
+	TrangThai bit,
 	foreign key (NhanVienTrucCa) references NhanVien(MaNV)
+)
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[NhanVien]') AND type in (N'U'))
+DROP TABLE [dbo].CaLam
+
+go
+create table CaLam(
+	MaCaLam nvarchar(10) PRIMARY KEY,
+	MaNV nvarchar(10), 
+	TrangThai bit,
+	MaCa nvarchar(10),
+	GhiChu nvarchar(50),
+	foreign key (MaNV) references NhanVien(MaNV),
+	foreign key (MaCa) references CaLamViec(MaCLV)
 )
 
 go
@@ -89,6 +107,7 @@ create table HoaDonChi(
 	NguoiTao nvarchar(10),
 	TongTienChi float,
 	GhiChu nvarchar(50),
+	TrangThai nvarchar(50),
 	foreign key (NguoiTao) references NhanVien(MaNV)
 )
 
@@ -107,103 +126,82 @@ create table HoaDon(
 	ChiPhiKhac float,
 	HinhThucTT bit,
 	GhiChu nvarchar(50),
+	TrangThai nvarchar(50),
 	foreign key (NguoiTao) references NhanVien(MaNV),
 	foreign key (SanPham) references SanPham(MaSP),
 	foreign key (MaKM) references KhuyenMai(MaKM)
 )
+------------------------------------------
+go
 
-------------------------------------------------------
-INSERT INTO [dbo].[NhanVien]
-           ([MaNV]
-           ,[TenNV]
-           ,[Email]
-           ,[SDT]
-           ,[MatKhau]
-           ,[NgayVaoLam]
-           ,[GhiChu]
-           ,[GioiTinh]
-           ,[TrangThai]
-           ,[ChucVu])
-     VALUES
-           ('phucnh',N'Nguyễn Hữu Phúc','phucnh@fpt.edu.vn','0989861544','123','11-11-2012',N'Chưa có',0,N'Đang hoạt động',1)
-GO
--------------------
 
-INSERT INTO [dbo].[LoaiSanPham]
-           ([MaLSP]
-           ,[TenLSP]
-           ,[MoTa])
-     VALUES
-           ('MaLSP01',N'Thức uống',N'Không có')
-GO
----------------------
-INSERT INTO [dbo].[KhuyenMai]
-           ([MaKM]
-           ,[TenKM]
-           ,[NgayBD]
-           ,[NgayKT]
-           ,[GiaKM]
-           ,[GhiChu])
-     VALUES
-           ('KM01','Du an','2023-11-03','2023-12-15',200,N'Chưa có')
-GO
------------------
-INSERT INTO [dbo].[SanPham]
-           ([MaSP]
-           ,[TenSP]
-           ,[SoLuong]
-           ,[HinhAnh]
-           ,[Gia]
-           ,[GhiChu]
-           ,[MaLSP]
-           ,[MaKM])
-     VALUES
-           
-		   ('SP01',N'Trà lai',2,null,300000,N'Chưa có','MaLSP01','KM01')
-GO
+--------------
+go
+CREATE PROCEDURE ThongKeHoaDonChiTheoNgay
+@NgayTinh datetime
+AS
+BEGIN
+    SELECT *
+    FROM HoaDonChi
+    WHERE CONVERT(date, ThoiGianTao) = CONVERT(date, @NgayTinh)
+END
+ EXEC ThongKeHoaDonChiTheoNgay '2023-01-01'
+ -----------------
+
+ go
+ CREATE PROCEDURE ThongKeHoaDonTheoNgay
+@NgayTinh datetime
+AS
+BEGIN
+    SELECT *
+    FROM HoaDon
+    WHERE CONVERT(date, ThoiGianTao) = CONVERT(date, @NgayTinh)
+END
+EXEC ThongKeHoaDonTheoNgay '2023-01-05'
+----------------------
+
+go
+CREATE PROCEDURE ThongKeHoaDonChiTheoThang
+@ThangTinh date
+AS
+BEGIN
+    SELECT *
+    FROM HoaDonChi
+    WHERE FORMAT(ThoiGianTao, 'yyyy-MM') = FORMAT(@ThangTinh, 'yyyy-MM')
+END
+EXEC ThongKeHoaDonChiTheoThang '2023-01-01'
+------------------
+go
+CREATE PROCEDURE ThongKeHoaDonTheoThang
+@ThangTinh date
+AS
+BEGIN
+    SELECT *
+    FROM HoaDon
+    WHERE FORMAT(ThoiGianTao, 'yyyy-MM') = FORMAT(@ThangTinh, 'yyyy-MM')
+END
+EXEC ThongKeHoaDonTheoThang '2023-01-01'
+
+-----------------------------
+go
+CREATE PROCEDURE ThongKeHoaDonChiTheoNam
+@NamTinh int
+AS
+BEGIN
+    SELECT *
+    FROM HoaDonChi
+    WHERE YEAR(ThoiGianTao) = @NamTinh
+END
+EXEC ThongKeHoaDonChiTheoNam 2023
+
 -----------------------
-INSERT INTO [dbo].[CaLamViec]
-           ([MaCLV]
-           ,[TenCLV]
-           ,[NhanVienTrucCa]
-           
-           ,[Thu]
-           ,[GhiChu])
-     VALUES
-           
-		   ('Ca1','Ca 1','phucnh','Monday',N'Chưa có')
-GO
----------------------
-
-INSERT INTO [dbo].[HoaDonChi]
-           ([ThoiGianTao]
-           ,[NguoiTao]
-           ,[TongTienChi]
-           ,[GhiChu])
-     VALUES
-           
-		   ('2023-11-04','phucnh',150000,N'Chưa có')
-GO
------------------------
-INSERT INTO [dbo].[HoaDon]
-           ([MaHD]
-           ,[ThoiGianTao]
-           ,[NguoiTao]
-           ,[SanPham]
-           ,[TongTien]
-           ,[MaKM]
-           ,[ChiPhiKhac]
-           ,[HinhThucTT]
-           ,[GhiChu])
-     VALUES
-           
-		   ('HD01','2023-11-05','phucnh','SP01',120000,'KM01',0,0,N'Chưa có')
-GO
-
-select * from NhanVien
-select * from CaLamViec
-select * from HoaDon
-select * from HoaDonChi
-select * from KhuyenMai
-select * from LoaiSanPham
-select * from SanPham
+go
+CREATE PROCEDURE ThongKeHoaDonTheoNam
+@NamTinh int
+AS
+BEGIN
+    SELECT *
+    FROM HoaDon
+    WHERE YEAR(ThoiGianTao) = @NamTinh
+END
+EXEC ThongKeHoaDonTheoNam 2023
